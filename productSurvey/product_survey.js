cf13893a0979d6ee4b341549512543dfa892fa05
senkja <senkja@gmail.com>
2024-11-24 12:20:51 -0500

    Added Product Survey Practice

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -10,6 +10,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experience = document.getElementById('experienceText').value;
 
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -17,7 +18,9 @@ function submitFeedback() {
     document.getElementById('userJob').innerHTML = job;
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
+    document.getElementById('userProductChoice2').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = experience;
     alert('Thank you for your valuable feedback');
     document.getElementById('userInfo').style.display = 'block';
 
