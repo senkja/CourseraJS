094396cdc097faf663119fe7483bfad395cd2b31
senkja <senkja@gmail.com>
2024-11-24 10:38:40 -0500

    Added authenticated

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -45,3 +45,6 @@ switch (userType) {
 
 console.log("User Category:", userCategory);
 
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+console.log("Authentication Status:", authenticationStatus);
