b4e81d7aaa5e8d08a014983f44c09a15b4dc95bf
senkja <senkja@gmail.com>
2024-11-27 15:35:02 -0500

    Added Total Length

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -30,6 +30,8 @@ function startTest() {
         return word !== "";
     }).length;
 
+    var totalLength = userTypedText.length;
+
     var wpm = 0; // Default value
 
     if (timeElapsed !== 0 && !isNaN(typedWords)) {
@@ -39,6 +41,7 @@ function startTest() {
     // Display the results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+        "<p>Total Length: " + totalLength + "</p>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
